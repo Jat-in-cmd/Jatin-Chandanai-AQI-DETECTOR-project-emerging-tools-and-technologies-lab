cb54c0e049b1d3eb7a7a4a9038c2961130c1c79e
Jat-in-cmd <chandnanijatin16@gmail.com>
2026-03-12 01:02:39 +0530

    Update main.py

--- Air-Quality-Monitor/src/main.py
+++ Air-Quality-Monitor/src/main.py
@@ -3,8 +3,18 @@ Air Quality Monitor
 Basic project initialization file
 """
 
+from sensor_simulator import generate_sensor_data
+
+
 def main():
     print("Air Quality Monitoring System Initialized")
+
+    data = generate_sensor_data()
+
+    print("Simulated Sensor Data:")
+    for key, value in data.items():
+        print(f"{key}: {value}")
+
 
 if __name__ == "__main__":
     main()

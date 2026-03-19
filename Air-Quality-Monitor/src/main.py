497d34b6f2a302a9c84323e2636616dd74655b84
Jat-in-cmd <chandnanijatin16@gmail.com>
2026-03-19 09:53:44 +0530

    Update main.py

--- Air-Quality-Monitor/src/main.py
+++ Air-Quality-Monitor/src/main.py
@@ -4,16 +4,24 @@ Basic project initialization file
 """
 
 from sensor_simulator import generate_sensor_data
+from data_handler import save_data
+from aqi_calculator import calculate_aqi
 
 
 def main():
-    print("Air Quality Monitoring System Initialized")
+    print("Air Quality Monitoring System Running...\n")
 
     data = generate_sensor_data()
 
-    print("Simulated Sensor Data:")
+    aqi = calculate_aqi(data)
+    data["AQI"] = aqi
+
+    print("Sensor Data:")
     for key, value in data.items():
         print(f"{key}: {value}")
+
+    save_data(data)
+    print("\nData saved successfully!")
 
 
 if __name__ == "__main__":
